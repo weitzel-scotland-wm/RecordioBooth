a9ebca7233f25420f55a49b6d9075b571c28697b
test <session2@START-Coding-MBP-20.local>
2015-12-17 16:16:35 -0700

    Initial commit: starting from when we left off!

---
+++ Html/action.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: session2
+ * Date: 12/9/15
+ * Time: 4:56 PM
+ */
+$message = "This function won't be available yet, until we can get php servers working to upload this data in to Apache. Sorry about that!";
+echo "<script type='text/javascript'>alert('$message');</script>";
