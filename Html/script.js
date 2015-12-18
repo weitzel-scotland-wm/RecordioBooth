5895afadcba193bd521889b98cb24e3088685b21
Scotland Weitzel <scotland.weitzel@west-mec.org>
2015-12-17 17:37:28 -0700

    Removed static image of Google Maps in FindUs.html Tried integrating actual google maps plug-in [NOT WORKING]

--- Html/script.js
+++ Html/script.js
@@ -43,3 +43,14 @@ $(function(){
     setInterval(doScroll, 20);
     tweenToNewSpeed(controller.fullSpeed);
 });
+
+function initialize() {
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+        center: new google.maps.LatLng(44.5403, -78.5463),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+}
+google.maps.event.addDomListener(window, 'load', initialize);
